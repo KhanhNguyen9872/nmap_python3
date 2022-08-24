b4de19ef6b126cdb4229ed9b9a32b5ac29b8498b
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-08-25 06:24:15 +0700

    Add files via upload

--- nmap.py
+++ nmap.py
@@ -8,15 +8,14 @@ global open
 open=[]
 max=0
 timeout=999
-
 def kill_process():
     print("\nClosing process....")
     if (name == 'nt'):
         system("taskkill /f /im python.exe >null 2>&1")
     else:
+        system("kill -9 $(pgrep python3) >/dev/null 2>&1")
         system("killall python3 >/dev/null 2>&1")
     exit()
-
 def check_ip(host,port):
     s = socket.socket()
     try:
@@ -26,7 +25,6 @@ def check_ip(host,port):
     else:
         s.close()
         open.append(f"{port}")
-
 host=str(input("IP: "))
 while (max < 1) or (max > 65535):
     try:
@@ -36,7 +34,6 @@ while (max < 1) or (max > 65535):
     except:
         print("\n Integer number [1-65535]\n Large port can take quite a while\n")
         max=0
-
 while (timeout < 0) or (timeout > 10):
     try:
         timeout=float(input("Time port (Default: 0.002): "))
@@ -45,7 +42,6 @@ while (timeout < 0) or (timeout > 10):
     except:
         print("\n Float number [0-10] (Default: 0.002)\n Short time may cause the device to lag\n")
         timeout=999
-
 print("\nScanning...")
 for port in range(1,max+1,1):
     try:
@@ -56,7 +52,6 @@ for port in range(1,max+1,1):
         sleep(timeout)
     except KeyboardInterrupt:
         kill_process()
-
 print("\nScan complete!")
 sleep(6)
 open = str(", ".join(open))
