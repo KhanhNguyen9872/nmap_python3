8dcee443d72bcc655d21a95a3f699db3c333763d
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-08-24 19:27:45 +0700

    first commit

---
+++ nmap.py
@@ -0,0 +1,54 @@
+from time import sleep
+from threading import Thread
+from os import name
+from os import system
+import socket
+from sys import stdout
+global open
+open=[]
+max=0
+
+def kill_process():
+    if (name == 'nt'):
+        system("taskkill /f /im python.exe >null 2>&1")
+    else:
+        system("killall python3 >/dev/null 2>&1")
+    exit()
+
+def check_ip(host,port):
+    s = socket.socket()
+    try:
+        s.connect((host, int(port)))
+    except:
+        pass
+    else:
+        s.close()
+        open.append(f"{port}")
+
+host=str(input("IP: "))
+while (max == 0) or (max < 1) or (max > 65535):
+    try:
+        max=int(input("Max port scan: "))
+    except KeyboardInterrupt:
+        kill_process()
+    except:
+        print("\n Input number [1-65535]\n Large port can take quite a while\n")
+        max=0
+
+print("Scanning...")
+for port in range(1,max+1,1):
+    try:
+        Thread(target=check_ip, args=(host,port)).start()
+        i=int(100/max*port)
+        stdout.write("\r[%-50s] %d%%" % ('='*int(i/2), i))
+        stdout.flush()
+        sleep(0.001)
+    except KeyboardInterrupt:
+        kill_process()
+
+print("\nScan complete!")
+sleep(6)
+open = str(", ".join(open))
+print(f"\nPort open: {open}")
+print("\nClosing process....")
+kill_process()
